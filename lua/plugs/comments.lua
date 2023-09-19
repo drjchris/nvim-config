9de40e23060892aa1f2501559b64dd4f419f2a78
drJosChris <jose.a.christian@gmail.com>
2023-09-19 14:18:45 +0700

    line comment in v mode

--- lua/plugs/comments.lua
+++ lua/plugs/comments.lua
@@ -8,7 +8,7 @@ require('Comment').setup({
     ---LHS of operator-pending mappings in NORMAL and VISUAL mode
     opleader = {
         ---Line-comment keymap
-        line = 'cl',
+        line = '<leader>//',
         ---Block-comment keymap
         block = '<leader>/b',
     },
